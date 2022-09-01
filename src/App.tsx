68b6994934113f619bf8ff19a551253888c7c0e9
Artyom <artempurya@gmail.com>
2022-09-01 21:34:32 +0300

    test the file eslinter for Evgeniy

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Testa } from "./test";
 
 function App() {
+  console.error("ok");
+  console.log('ok');
+
   return (
     <div className="App">
-
+      <Testa />
     </div>
   );
 }
